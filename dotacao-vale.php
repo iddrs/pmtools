96423771f6951bb2ea778a282c8222b1da632020
Everton da Rosa <everton3x@gmail.com>
2024-08-20 14:32:03 -0300

    feat: Implementado script da dotação do vale-alimentação

--- dotacao-vale.php
+++ dotacao-vale.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Calcula o superavit ou deficit da dotação para folha de pagamento.
+ * Calcula o superavit ou deficit da dotação para vale-alimentação.
  * 
  */
 
@@ -14,7 +14,7 @@ $remessa = read_remessa();
 notice("Remessa selecionada: {$remessa->remessa}.");
 
 // Calcula a dotação atualizada
-$sql = "select sum(dotacao_atualizada)::decimal as dotacao_atualizada from pad.bal_desp where remessa = $1 and (elemento like '31%%' or elemento like '319007%%' or elemento like '339008%%') and entidade like 'pm'";
+$sql = "select sum(dotacao_atualizada)::decimal as dotacao_atualizada from pad.bal_desp where remessa = $1 and elemento like '339046%%' and entidade like 'pm'";
 $result = pg_query_params(connect(), $sql, [$remessa->remessa]);
 $dotacao_atualizada = round(pg_fetch_result($result, 0, 0), 2);
 
@@ -22,7 +22,7 @@ notice(sprintf('Dotação atualizada: %s', fmt_currency($dotacao_atualizada)));
 
 
 // Calcula o valor empenhado até a data-base
-$sql = "select sum(valor_empenhado)::decimal as empenhado from pad.bal_desp where remessa = $1 and (elemento like '31%%' or elemento like '319007%%' or elemento like '339008%%') and entidade like 'pm'";
+$sql = "select sum(valor_empenhado)::decimal as empenhado from pad.bal_desp where remessa = $1 and elemento like '339046%%' and entidade like 'pm'";
 $result = pg_query_params(connect(), $sql, [$remessa->remessa]);
 $empenhado = round(pg_fetch_result($result, 0, 0), 2);
 
@@ -30,68 +30,20 @@ notice(sprintf('Empenhado até %s: %s', $remessa->dataBase->format('d/m/Y'), fmt
 
 
 // Calcula o valor empenhado no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and (rubrica like '31%%' or rubrica like '319007%%' or rubrica like '339008%%') and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
+$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and rubrica like '339046%%' and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
 $result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
 $empenhado_mes = round(pg_fetch_result($result, 0, 0), 2);
 
 notice(sprintf('Empenhado de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($empenhado_mes)));
 
 
-// Calcula o 1/3 de férias empenhado no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and (rubrica like '31901142%%' or rubrica like '31901145%%') and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
-$result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
-$terco_ferias_empenhado_mes = round((float) pg_fetch_result($result, 0, 0), 2);
-
-notice(sprintf('1/3 de férias empenhado de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($terco_ferias_empenhado_mes)));
-
-
-// Calcula o prêmio assiduidade empenhado no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and rubrica like '31901147%%' and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
-$result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
-$premio_assiduidade_empenhado_mes = round((float) pg_fetch_result($result, 0, 0), 2);
-
-notice(sprintf('Prêmio assiduidade empenhado de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($premio_assiduidade_empenhado_mes)));
-
-
-// Calcula o 13º salário empenhado no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and rubrica like '31901143%%' and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
-$result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
-$decimo_empenhado_mes = round((float) pg_fetch_result($result, 0, 0), 2);
-
-notice(sprintf('13º salário empenhado de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($decimo_empenhado_mes)));
-
-
-// Calcula as sentenças judiciais empenhadas no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and rubrica like '319091%%' and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
-$result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
-$judicial_empenhado_mes = round((float) pg_fetch_result($result, 0, 0), 2);
-
-notice(sprintf('Sentenças judiciais empenhadas de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($judicial_empenhado_mes)));
-
-
-// Calcula as despesas de exercícios anteriores empenhadas no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and rubrica like '319092%%' and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
-$result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
-$dea_empenhado_mes = round((float) pg_fetch_result($result, 0, 0), 2);
-
-notice(sprintf('Despesas de exercícios anteriores empenhadas de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($dea_empenhado_mes)));
-
-
-// Calcula as indenizações empenhadas no mês
-$sql = "select sum(valor_empenho)::decimal as empenhado from pad.empenho where remessa = $1 and rubrica like '319094%%' and entidade like 'pm' and ano_empenho <= $2 and data_empenho between $3 and $4";
-$result = pg_query_params(connect(), $sql, [$remessa->remessa, $remessa->ano, $remessa->inicioDoMes->format('Y-m-d'), $remessa->dataBase->format('Y-m-d')]);
-$indenizacao_empenhado_mes = round((float) pg_fetch_result($result, 0, 0), 2);
-
-notice(sprintf('Indenizações empenhadas de %s até %s: %s', $remessa->inicioDoMes->format('d/m/Y'), $remessa->dataBase->format('d/m/Y'), fmt_currency($indenizacao_empenhado_mes)));
-
-
 // Calcula o valor mensal base para empenho
-$empenhar_mensal = round($empenhado_mes - $terco_ferias_empenhado_mes - $premio_assiduidade_empenhado_mes - $decimo_empenhado_mes - $judicial_empenhado_mes - $dea_empenhado_mes - $indenizacao_empenhado_mes, 2);
-notice(sprintf('Valor mensal a empenhar: %s', fmt_currency($dea_empenhado_mes)));
+$empenhar_mensal = round($empenhado_mes, 2);
+notice(sprintf('Valor mensal a empenhar: %s', fmt_currency($empenhar_mensal)));
 
 
 // Calcula os meses a empenhar
-$meses_a_empenhar = 13 - $remessa->mes;
+$meses_a_empenhar = 12 - $remessa->mes;
 notice("Meses a empenhar: $meses_a_empenhar");
 
 
