d56a24302ad88429c44b8e8579b883d10e63d59e
Everton da Rosa <everton3x@gmail.com>
2024-08-21 14:28:54 -0300

    feat: Incluído cálculo da arrecadação da dívida ativa.

---
+++ receita-divida-ativa.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Calcula o valor das transferências da saúde.
+ * 
+ * 
+ */
+
+require 'vendor/autoload.php';
+
+printnl('=======================================================================');
+printnl('RECEITA DA DÍVIDA ATIVA');
+printnl('Valor acumulado no ano da receita de dívida ativa.');
+printnl('=======================================================================');
+printnl('');
+printnl('');
+
+$cfg = load_config();
+
+$remessa = read_remessa();
+
+notice("Remessa selecionada: {$remessa->remessa}.");
+
+// Calcula a dívida tributária
+$sql = "SELECT
+    sum(receita_realizada)::numeric AS arrecadado
+   FROM pad.bal_rec
+  WHERE tipo_nivel_receita ~~ 'A'::text AND tipo_receita in (3, 4) AND natureza_receita::text ~~ '11%'::text AND entidade::text ~~ 'pm'::text
+  AND remessa = $1;";
+$result = pg_query_params(connect(), $sql, [$remessa->remessa]);
+$tributaria = pg_fetch_assoc($result, 0)['arrecadado'];
+
+
+// Calcula a dívida não tributária
+$sql = "SELECT
+    sum(receita_realizada)::numeric AS arrecadado
+   FROM pad.bal_rec
+  WHERE tipo_nivel_receita ~~ 'A'::text AND tipo_receita in (3, 4) AND natureza_receita::text !~~ '11%'::text AND entidade::text ~~ 'pm'::text
+  AND remessa = $1;";
+$result = pg_query_params(connect(), $sql, [$remessa->remessa]);
+$nao_tributaria = pg_fetch_assoc($result, 0)['arrecadado'];
+
+printnl('');
+printnl('');
+
+// Resumo
+printnl('=======================================================================');
+printnl('RESULTADO');
+printnl('-----------------------------------------------------------------------');
+printnl("Dívida\t\t\tArrecadado");
+printnl(sprintf("%s\t\t%s", 'Tributária', fmt_currency($tributaria)));
+printnl(sprintf("%s\t\t%s", 'Não Tributária', fmt_currency($nao_tributaria)));
+printnl('-----------------------------------------------------------------------');
+
+
+
+printnl('=======================================================================');
+
+notice('Processo terminado!');
