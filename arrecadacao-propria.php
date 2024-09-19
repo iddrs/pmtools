338149a64273807988c85d98890d5b40fe034507
Everton da Rosa <everton3x@gmail.com>
2024-09-19 08:34:38 -0300

    feat: Cálculo da arrecadação própria.
    Incluído valor da arrecadação no mês.

--- arrecadacao-propria.php
+++ arrecadacao-propria.php
@@ -34,10 +34,22 @@ $sql = "SELECT
     AND natureza_receita::text !~~ '17%'::text
     AND entidade::text ~~ 'pm'::text
     AND remessa = $1;";
+
+// Acumulado da remessa
 $result = pg_query_params(connect(), $sql, [$remessa->remessa]);
 $previsao_inicial = pg_fetch_assoc($result, 0)['previsao_inicial'];
 $previsao_atualizada = pg_fetch_assoc($result, 0)['previsao_atualizada'];
 $arrecadado = pg_fetch_assoc($result, 0)['arrecadado'];
+
+// Acumulado da remessa anterior
+if($remessa->mes === 1){
+    $arrecadado_anterior = 0.0;
+}else{
+    $remessa_anterior = sprintf('%s%02s', $remessa->ano, $remessa->mes - 1);
+    $result = pg_query_params(connect(), $sql, [$remessa_anterior]);
+    $arrecadado_anterior = pg_fetch_assoc($result, 0)['arrecadado'];
+}
+$arrecadado_mes = $arrecadado - $arrecadado_anterior;
 
 printnl('');
 printnl('');
@@ -46,8 +58,9 @@ printnl('');
 printnl('=======================================================================');
 printnl('RESULTADO');
 printnl('-----------------------------------------------------------------------');
-printnl("Previsão Inicial\tPrevisão Atualizada\tArrecadado");
-printnl(sprintf("%s\t\t%s\t\t%s", fmt_currency($previsao_inicial), fmt_currency($previsao_atualizada), fmt_currency($arrecadado)));
+printnl("\t\tPrevisão Inicial\tPrevisão Atualizada\tArrecadado");
+printnl(sprintf("No mês\t\t\t\t%s\t\t%s\t\t%s", '', '', fmt_currency($arrecadado_mes)));
+printnl(sprintf("Acumulado\t%s\t\t%s\t\t%s", fmt_currency($previsao_inicial), fmt_currency($previsao_atualizada), fmt_currency($arrecadado)));
 printnl('-----------------------------------------------------------------------');
 
 
