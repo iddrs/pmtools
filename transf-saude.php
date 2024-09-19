e6a48885ce1da5c2d8738a26c7e003abb449b1d9
Everton da Rosa <everton3x@gmail.com>
2024-09-19 08:37:43 -0300

    feat: Transferências correntes da saúde
    Incluído cálculo do arrecadado mensal.

--- transf-saude.php
+++ transf-saude.php
@@ -29,7 +29,36 @@ $sql = "SELECT
    FROM pad.bal_rec
   WHERE tipo_nivel_receita ~~ 'A'::text AND fonte_recurso >= 600 AND fonte_recurso <= 659 AND natureza_receita::text ~~ '17%'::text AND entidade::text ~~ 'pm'::text
   AND remessa = $1;";
+
+// Acumulado da remessa
 $result = pg_query_params(connect(), $sql, [$remessa->remessa]);
+$previsao_inicial = pg_fetch_assoc($result, 0)['previsao_inicial'];
+$previsao_atualizada = pg_fetch_assoc($result, 0)['previsao_atualizada'];
+$arrecadado = pg_fetch_assoc($result, 0)['arrecadado'];
+
+// Acumulado da remessa anterior
+if($remessa->mes === 1){
+    $arrecadado_anterior = 0.0;
+}else{
+    $remessa_anterior = sprintf('%s%02s', $remessa->ano, $remessa->mes - 1);
+    $result = pg_query_params(connect(), $sql, [$remessa_anterior]);
+    $arrecadado_anterior = pg_fetch_assoc($result, 0)['arrecadado'];
+}
+$arrecadado_mes = $arrecadado - $arrecadado_anterior;
+
+printnl('');
+printnl('');
+
+// Resumo
+printnl('=======================================================================');
+printnl('RESULTADO');
+printnl('-----------------------------------------------------------------------');
+printnl("\t\tPrevisão Inicial\tPrevisão Atualizada\tArrecadado");
+printnl(sprintf("No mês\t\t\t\t%s\t\t%s\t\t%s", '', '', fmt_currency($arrecadado_mes)));
+printnl(sprintf("Acumulado\t%s\t\t%s\t\t%s", fmt_currency($previsao_inicial), fmt_currency($previsao_atualizada), fmt_currency($arrecadado)));
+printnl('-----------------------------------------------------------------------');
+
+/*$result = pg_query_params(connect(), $sql, [$remessa->remessa]);
 $previsao_inicial = pg_fetch_assoc($result, 0)['previsao_inicial'];
 $previsao_atualizada = pg_fetch_assoc($result, 0)['previsao_atualizada'];
 $arrecadado = pg_fetch_assoc($result, 0)['arrecadado'];
@@ -43,7 +72,7 @@ printnl('RESULTADO');
 printnl('-----------------------------------------------------------------------');
 printnl("Previsão Inicial\tPrevisão Atualizada\tArrecadado");
 printnl(sprintf("%s\t\t%s\t\t%s", fmt_currency($previsao_inicial), fmt_currency($previsao_atualizada), fmt_currency($arrecadado)));
-printnl('-----------------------------------------------------------------------');
+printnl('-----------------------------------------------------------------------');*/
 
 
 
